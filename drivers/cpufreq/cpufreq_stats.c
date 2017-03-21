aa217c17099e99ff370ac6158985a05bfa9519a3
Sultan Alsawaf <sultanxda@gmail.com>
2017-03-21 14:10:50 -0700

    cpufreq: stats: Fix memory leaks when updating stats table
    The address for an element in the per-cpu cpufreq_stats_table variable
    is overwritten in cpufreq_stats_update_policy_cpu(), but the memory
    allocated at the address that gets overwritten is not freed beforehand.
    
    Free the allocated memory beforehand to fix the memory leaks.
    
    Change-Id: Ia854e1ec8c080d27f9c2e4ab35e43b3d1d528b48
    Signed-off-by: Sultan Alsawaf <sultanxda@gmail.com>

--- drivers/cpufreq/cpufreq_stats.c
+++ drivers/cpufreq/cpufreq_stats.c
@@ -639,6 +639,15 @@ static void cpufreq_stats_update_policy_cpu(struct cpufreq_policy *policy)
 
 	pr_debug("Updating stats_table for new_cpu %u from last_cpu %u\n",
 			policy->cpu, policy->last_cpu);
+
+	stat = per_cpu(cpufreq_stats_table, policy->cpu);
+	if (stat) {
+		kfree(stat->time_in_state);
+		kfree(stat);
+	}
+
+	stat = per_cpu(cpufreq_stats_table, policy->last_cpu);
+
 	per_cpu(cpufreq_stats_table, policy->cpu) = per_cpu(cpufreq_stats_table,
 			policy->last_cpu);
 	per_cpu(cpufreq_stats_table, policy->last_cpu) = NULL;
