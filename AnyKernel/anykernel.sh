fd5e6e12cc7075c1b58162e04156af26526b4850
timangpopi <53063799+timangpopi@users.noreply.github.com>
2019-08-04 08:36:38 +0700

    Update anykernel.sh

--- AnyKernel/anykernel.sh
+++ AnyKernel/anykernel.sh
@@ -1,20 +1,21 @@
-# AnyKernel3 Ramdisk Mod Script
+# AnyKernel2 Ramdisk Mod Script
 # osm0sis @ xda-developers
+# Thanks to Mihran thalhath, coz i took this script from his no name kernel.
 
 ## AnyKernel setup
 # begin properties
-properties() { '
-kernel.string=SteelHeart-Q.1
+properties() {
+kernel.string=Hoi peru!!
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=Redmi 4a
-device.name2=Rolex
+device.name1=redmi4a
+device.name2=rolex
 device.name3=
-supported.versions=8.1 - 9.0
-supported.patchlevels=All support
-'; } # end properties
+device.name4=
+device.name5=
+} # end properties
 
 # shell variables
 block=/dev/block/bootdevice/by-name/boot;
@@ -23,9 +24,12 @@ ramdisk_compression=auto;
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
-. tools/ak3-core.sh;
+. /tmp/anykernel/tools/ak2-core.sh;
 
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+ui_print "";
 ## AnyKernel install
 dump_boot;
+##AnyKernel finish
 write_boot;
-## end install
